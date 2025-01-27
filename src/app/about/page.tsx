bfd5e3574256464f57dec685081c7291631f9f67
BenjaminVeli <benjamin.veli@tecsup.edu.pe>
2025-01-26 19:02:47 -0500

    Finished

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const AboutPage = () => {
-    return (
-        <div>Helo from AboutPage</div>
-    )
-}
-
-export default AboutPage;
--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -8,6 +8,7 @@ import { Textarea } from "./ui/textarea";
 import toast from "react-hot-toast";
 import { ImageIcon, Loader2Icon, SendIcon } from "lucide-react";
 import { createPost } from "@/actions/post.action";
+import ImageUpload from "./ImageUpload";
 
 function CreatePost() {
   const { user } = useUser();
@@ -22,7 +23,7 @@ function CreatePost() {
     setIsPosting(true);
     try {
       const result = await createPost(content, imageUrl);
-      if (result.success) {
+      if (result?.success) {
         setContent("");
         setImageUrl("");
         setShowImageUpload(false);
@@ -52,7 +53,17 @@ function CreatePost() {
           />
         </div>
 
-        {/* TODO: HANDLE IMAGE UPLOAD */}
+        {(showImageUpload || imageUrl) && (
+          <div className="border rounded-lg p-4">
+            <ImageUpload
+              endpoint="postImage"
+              value={imageUrl}
+              onChange={(url) => {
+                setImageUrl(url);
+                if(!url) setShowImageUpload(false);
+              }} />
+          </div>
+        )}
 
         <div className="flex items-center justify-between border-t pt-4">
           <div className="flex space-x-2">
